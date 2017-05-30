352c2e480af662d941d3b97a7419e440719eb423
nicolasm <nico.meignotte@gmail.com>
2017-05-30 16:30:26 +0200

    pagination almost workinf

--- core/src/com/nf28/game/ShopScreen.java
+++ core/src/com/nf28/game/ShopScreen.java
@@ -36,60 +36,63 @@ public class ShopScreen implements Screen {
     Skin skin;
     VocabularyQuest game;
     Stage stage;
+    int currentPage = 0;
+    List<String> listUrl = new ArrayList<String>();
+    List<String> listPrice = new ArrayList<String>();
+    List<String> listName = new ArrayList<String>();
+    List<Label> nameLabelList = new ArrayList<Label>();
+    List<Label> priceLabelList = new ArrayList<Label>();
+    List<Image> imageList = new ArrayList<Image>();
+
+
+    Table table;
 
     public ShopScreen(final VocabularyQuest game){
         this.game = game;
-
         stage=new Stage();
         Gdx.input.setInputProcessor(stage);
         skin = new Skin( Gdx.files.internal( "ui/defaultskin.json" ));
-
-
-        final Table table = new Table();
+        table = new Table();
         table.setFillParent(true);
-
-
         FileHandle file = Gdx.files.internal("character/character_list.json");
         String text = file.readString();
-
-
         table.setDebug(true);
         JsonValue json = new JsonReader().parse(text);
         JsonValue jsonobj = json.get("character");
         int cpt=1;
-        List<Integer> price = new ArrayList<Integer>();
-        List<String> name = new ArrayList<String>();
-        for (JsonValue o : jsonobj.iterator())
-        {
-            if(cpt==13)
-                break;
-            Image test = new Image();
-            test.setDrawable(new TextureRegionDrawable(new TextureRegion(new Texture("character/"+o.getString("url")))));
-            test.setScaling(Scaling.fit);
-            table.add(test).fill().expand().colspan(2);
-            price.add(o.getInt("price"));
-            name.add(o.getString("name"));
-            if(cpt++%3==0){
-                table.row();
-                table.add(new Label(name.get(0),skin)).expandX();
-                table.add(new Label("" + price.get(0), skin)).expandX();
-                table.add(new Label(name.get(1),skin)).expandX();
-                table.add(new Label("" + price.get(1), skin)).expandX();
-                table.add(new Label(name.get(2),skin)).expandX();
-                table.add(new Label("" + price.get(2), skin)).expandX();
-                table.row();
-                name.remove(0);
-                name.remove(0);
-                name.remove(0);
-                price.remove(0);
-                price.remove(0);
-                price.remove(0);
-            }
-
+        for (JsonValue o : jsonobj.iterator()) {
+            listUrl.add(o.getString("url"));
+            listPrice.add("" + o.getInt("price"));
+            listName.add(o.getString("name"));
         }
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                Image image = new Image();
+                //test.setDrawable(new TextureRegionDrawable(new TextureRegion(new Texture("character/" + o.getString("url")))));
+                image.setScaling(Scaling.fit);
+                table.add(image).fill().expand().colspan(2);
+                imageList.add(image);
+            }
+            table.row();
+            for(int j=0;j<3;j++) {
+                Label nameLabel = new Label("name",skin);
+                table.add(nameLabel).fillX().expandX();
+                nameLabelList.add(nameLabel);
+                Label priceLabel = new Label("name",skin);
+                //test.setDrawable(new TextureRegionDrawable(new TextureRegion(new Texture("character/" + o.getString("url")))));
+                table.add(priceLabel).fillX().expandX();
+                priceLabelList.add(priceLabel);
+            }
+            table.row();
+        }
+        refresh(currentPage);
 
+        TextButton button_left =new TextButton("<",skin);
+        table.add(button_left).height(100).expand().fill();
+        TextButton button_right =new TextButton(">",skin);
+        table.add(button_right).height(100).expand().fill();
         final TextButton retour =new TextButton("Retour",skin);
-        table.add(retour).expandX().fillX();
+        table.add(retour).expand().fill();
         table.row();
         retour.addListener(new ClickListener() {
             @Override
@@ -98,12 +101,43 @@ public class ShopScreen implements Screen {
                 game.setScreen(game.mainMenuScreen);
             }
         });
+        button_left.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(currentPage-1>=0){
+                    currentPage--;
+                    refresh(currentPage);
+                }
+
+            }
+        });
+        button_right.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                currentPage++;
+                refresh(currentPage);
+            }
+        });
         stage.addActor(table);
 
 
 
     }
-
+    public void refresh(int page){
+        for(Image i : imageList)
+            i.setDrawable(null);
+        for(Label l:nameLabelList)
+            l.setText("");
+        for(Label l:priceLabelList)
+            l.setText("");
+        for(int i=0;i<9;i++){
+            if(page * 9 + i == listPrice.size())
+                break;
+            imageList.get(i).setDrawable(new TextureRegionDrawable(new TextureRegion(new Texture("character/"+listUrl.get(page*9+i)))));
+            nameLabelList.get(i).setText(listName.get(page * 9 + i));
+            priceLabelList.get(i).setText(listPrice.get(page * 9 + i));
+        }
+    }
     @Override
     public void show() {
 
